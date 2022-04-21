7e8e7bdfe2d619e616dbe00408602ce4269aad1d
Anson_kyk <anson.kong@mail.utoronto.ca>
2022-04-21 11:55:55 -0400

    call it a day

--- main.js
+++ main.js
@@ -31,9 +31,58 @@ async function add(event){
         const rawData = await fetch(
            `https://api.polygon.io/v2/aggs/ticker/${stock}/range/1/day/${startdate}/${today}?adjusted=true&sort=asc&apiKey=${apiKey}`
         );
+        stockclosing=[];
         const stockdata = await rawData.json();
         console.log(stockdata);
+        for (var j = 0; j < stockdata.results.length; j++){
+            stockclosing.push(stockdata.results[j].c);
+            
+        }
+        console.log(stockclosing)
+        //Output some of the highlight
+        document.getElementById("latestprice").innerHTML=stockclosing[stockclosing.length-1];
+        var rev=(stockclosing[stockclosing.length-1]-stockclosing[0])/stockclosing[0];
+        document.getElementById("lastyearrev").innerText=rev;
+        var fakedate=[...Array(stockclosing.length).keys()];
+        const data = {
+            labels: fakedate,
+            datasets: [{
+              label: 'Last year stock data',
+              data: stockclosing,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                
+              ],
+              borderColor: [
+                'rgb(255, 99, 132)',
+                
+              ],
+              borderWidth: 1
+            }]
+          };
+          
+          
+          const config = {
+              type: 'line',
+              data: data,
+              options: {
+                scales: {
+                  y: {
+                    beginAtZero: true
+                  }
+                }
+              },
+            };
+          
+          
+          
+            const myChart2 = new Chart(
+              document.getElementById('stockchart'),
+              config
+            );
 
+
+        
     }else{
         alert('error input!!')
     }
@@ -69,3 +118,4 @@ function lastyeardategetter(){
     today = yyyy + '-' + mm + '-' + dd;
     return today;
 }
+
